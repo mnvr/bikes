2b4ba2da4b9eb930db0418ebad4d99b4949c46a0
Manav Rathi <mnvrth@gmail.com>
2019-04-09 09:50:25 +0300

    Initial commit

---
+++ Bikes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bikes
+//
+//  Created by Manav Rathi on 09/04/2019.
+//  Copyright © 2019 Manav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Helsinki City Bikes Near Me
+
+An iOS app to list the Helsinki city bikes near your location.
+
+Unofficial, not affiliated to anyone; made for my personal use.
+
+WIP.
+
+## License
+
+All source code is licensed under the [MIT License](LICENSE).
