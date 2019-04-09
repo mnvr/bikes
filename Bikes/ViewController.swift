423043cd938af2517522b5b5722a6826995497b5
Manav Rathi <mnvrth@gmail.com>
2019-04-09 12:33:51 +0300

    Show pins

--- Bikes/ViewController.swift
+++ Bikes/ViewController.swift
@@ -63,6 +63,52 @@ class ViewController: UIViewController {
 
         let bikeRentalStations = response?.data?.bikeRentalStations
 
+        // MapKit terminology:
+        // - An annotation is a point.
+        // - An overlay is a region.
+
+        // Convert each of the stations into an annotation
+
+        var annotations = [MKAnnotation]()
+
+        for bikeRentalStation in bikeRentalStations ?? [] {
+            guard let lat = bikeRentalStation.lat,
+                let lon = bikeRentalStation.lon else {
+                    continue
+            }
+
+            // The current API documentation states that non realtime
+            // entries will always report a fixed number of bikes. So
+            // if the station is not realtime, display a "?" to the
+            // user instead of potentially misleading information (the
+            // user might walk there and feel angry that what was
+            // promised was not delivered).
+
+            let title: String?
+            if let bikesAvailable = bikeRentalStation.bikesAvailable,
+                let realtime = bikeRentalStation.realtime,
+                realtime == true {
+                title = "\(bikesAvailable)"
+            } else {
+                title = "?"
+            }
+
+            let subtitle = bikeRentalStation.name
+
+            let latitude = CLLocationDegrees(lat)
+            let longitude = CLLocationDegrees(lon)
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = title
+            annotation.subtitle = subtitle
+
+            annotations.append(annotation)
+        }
+
+        mapView?.showAnnotations(annotations, animated: true)
+
         NSLog("\(String(describing: bikeRentalStations))")
     }
 }
