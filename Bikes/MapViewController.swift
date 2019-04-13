3f2731d702ef9d975e318aa6f78232eba627de52
Manav Rathi <mnvrth@gmail.com>
2019-04-13 20:40:28 +0300

    Use the user's location

--- Bikes/MapViewController.swift
+++ Bikes/MapViewController.swift
@@ -12,6 +12,8 @@ class MapViewController: UIViewController {
 
     private var mapView: MKMapView?
     private var activityIndicatorView: UIActivityIndicatorView?
+    private var locationManager: CLLocationManager?
+    private var didRequestLocation = false
 
     let markerAnnotationViewReuseIdentifier = String(describing: MKMarkerAnnotationView.self)
 
@@ -43,20 +45,22 @@ class MapViewController: UIViewController {
             activityIndicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
             ])
 
+        let locationManager = CLLocationManager()
+        self.locationManager = locationManager
+        locationManager.delegate = self
+
         // SVG Icon Source: https://github.com/apancik/public-domain-icons
         // Saved to "location.svg".
         //
         // SVG converted to PDF using: https://cloudconvert.com/svg-to-pdf
         //
-        // Thank you 💯
-
         let locationIconImage = UIImage(imageLiteralResourceName: "Location").withRenderingMode(.alwaysTemplate)
 
         let locateMeButton = UIButton(type: .system)
         locateMeButton.translatesAutoresizingMaskIntoConstraints = false
         locateMeButton.setImage(locationIconImage, for: .normal)
         locateMeButton.adjustsImageSizeForAccessibilityContentSizeCategory = true
-        locateMeButton.addTarget(self, action: #selector(locateMe), for: .touchUpInside)
+        locateMeButton.addTarget(self, action: #selector(maybeLocateMe), for: .touchUpInside)
 
         view.addSubview(locateMeButton)
         NSLayoutConstraint.activate([
@@ -66,8 +70,12 @@ class MapViewController: UIViewController {
             locateMeButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
             ])
 
-        // TODO: Hide for now
-        locateMeButton.isHidden = true
+        mapView.showsUserLocation = true
+
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            locateMeWhenAuthorized()
+        }
 
         refresh()
     }
@@ -158,13 +166,89 @@ class MapViewController: UIViewController {
             annotations.append(annotation)
         }
 
-        mapView?.showAnnotations(annotations, animated: true)
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways {
+            // We might be already showing the map centered on the user's
+            // location. So do not change the current viewport of the
+            // map, instead just drop the annotations into whatever the
+            // map is currently showing.
+
+            mapView?.addAnnotations(annotations)
+        } else {
+            // If we do not have location access, use the showAnnotations
+            // methods that zooms the map such that it fits the
+            // given annotations.
+            //
+            // By default, when we don't have access, then the map
+            // shows the entirety of Finland. So this effectively
+            // zooms into the Helsinki region.
+
+            mapView?.showAnnotations(annotations, animated: true)
+        }
 
         NSLog("\(String(describing: bikeRentalStations))")
     }
 
-    @objc private func locateMe() {
+    @objc private func maybeLocateMe() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            locateMeWhenAuthorized()
 
+        case .notDetermined:
+            locationManager?.requestWhenInUseAuthorization()
+
+        case .denied, .restricted:
+            locateMeWhenNotAuthorized()
+
+        @unknown default:
+            break
+        }
+    }
+
+    private func locateMeWhenNotAuthorized() {
+        // Show an alert to the user telling them that they need to
+        // give access to the app in settings for us to be able to
+        // locate them.
+        //
+        // On the alert show a button to open settings.
+
+        let title = NSLocalizedString("no_location_alert_title", comment: "")
+        let message = NSLocalizedString("no_location_alert_message", comment: "")
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let openSettingsTitle = NSLocalizedString("no_location_alert_open_settings", comment: "")
+        alertController.addAction(UIAlertAction(title: openSettingsTitle, style: .default, handler: { _ in
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(url)
+            }
+        }))
+
+        let cancelTitle = NSLocalizedString("cancel", comment: "")
+        alertController.addAction(UIAlertAction(title: cancelTitle, style: .cancel, handler: nil))
+
+        present(alertController, animated: true)
+    }
+
+    private func locateMeWhenAuthorized() {
+        // Redundant check, but do it anyways.
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        guard authorizationStatus == .authorizedWhenInUse || authorizationStatus == .authorizedAlways else {
+            return
+        }
+
+        guard CLLocationManager.locationServicesEnabled() else {
+            // I don't know (yet) when we will reach here
+            // (and that doesn't mean we won't; in any case,
+            // this is what the Apple sample code tells us to do).
+            return
+        }
+
+        guard let userLocationCoordinate = locationManager?.location?.coordinate else {
+            return
+        }
+
+        let viewRegion = MKCoordinateRegion(center: userLocationCoordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView?.region = viewRegion
     }
 }
 
@@ -187,4 +271,34 @@ extension MapViewController: MKMapViewDelegate {
 
         return markerAnnotationView
     }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if !didRequestLocation {
+            // The docs state thet the authorization status might change
+            // without any action on our part (e.g. due to Airplane mode).
+            // https://developer.apple.com/documentation/corelocation/choosing_the_authorization_level_for_location_services/requesting_when-in-use_authorization
+            //
+            // So only do something in this method if we know it was
+            // called because of us calling
+            // locationManager?.requestWhenInUseAuthorization() above.
+            return
+        }
+
+        didRequestLocation = false
+
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            // If we are authorized, then what has happened is:
+            // - The user pressed the locate button
+            // - Said yes on the OS prompt
+            // - We come here
+            // So now do what they want.
+            locateMeWhenAuthorized()
+        default:
+            break
+        }
+    }
+
 }
