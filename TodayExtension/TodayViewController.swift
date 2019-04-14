e49f9d6fb26c2cff51da96cb8a3a0650c9487d9c
Manav Rathi <mnvrth@gmail.com>
2019-04-14 12:49:03 +0300

    Customize

--- TodayExtension/TodayViewController.swift
+++ TodayExtension/TodayViewController.swift
@@ -7,13 +7,34 @@
 import UIKit
 import NotificationCenter
 
-class TodayViewController: UIViewController, NCWidgetProviding {
-        
+class TodayViewController: UIViewController {
+    private var refreshButton: UIButton?
+    private var activityIndicatorView: UIActivityIndicatorView?
+    private var resultsLabel: UILabel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let button = UIButton(type: .system)
+        self.refreshButton = button
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("↻", for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .title1)
+        button.addTarget(self, action: #selector(refresh), for: .touchUpInside)
+
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
     }
-        
+
+    @objc private func refresh() {
+        NSLog("Refresh")
+    }
+}
+
+extension TodayViewController: NCWidgetProviding {
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         
@@ -23,5 +44,4 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         
         completionHandler(NCUpdateResult.newData)
     }
-    
 }
